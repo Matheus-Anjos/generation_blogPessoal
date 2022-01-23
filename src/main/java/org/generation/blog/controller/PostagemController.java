ba9dbdb8c8d26e565784b6e78d5acdf2ce481920
Matheus <matheus.mfa10@gmail.com>
2022-01-23 11:34:26 -0300

    FindById

--- src/main/java/org/generation/blog/controller/PostagemController.java
+++ src/main/java/org/generation/blog/controller/PostagemController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,4 +34,9 @@ public class PostagemController {
 		return ResponseEntity.ok(repository.findAll());
 	}
 	
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagem> GetById(@PathVariable Long id){
+		return repository.findById(id)
+				.map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+	}
 }
