e818c74a950ef6312ee7dde25ff50ad9d6e07881
Matheus <matheus.mfa10@gmail.com>
2022-02-01 15:22:40 -0300

    @Matheus-Anjos [Task : Criação Model Tema, TemaController e TemaRepository e Integração de Postagem e Tema]

---
+++ src/main/java/org/generation/blog/models/Tema.java
@@ -0,0 +1,61 @@
+
+package org.generation.blog.models;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+
+@Entity
+@Table(name = "tb_tema")
+
+public class Tema {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+    private String assunto;
+	
+	
+	@OneToMany(mappedBy = "tema", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("tema")
+	private List<Postagem> postagem;
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getAssunto() {
+		return assunto;
+	}
+
+	public void setAssunto(String assunto) {
+		this.assunto = assunto;
+	}
+
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
+
+	
+
+}
---
+++ src/main/java/org/generation/blog/repository/TemaRepository.java
@@ -0,0 +1,13 @@
+package org.generation.blog.repository;
+
+import java.util.List;
+
+import org.generation.blog.models.Tema;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TemaRepository extends JpaRepository< Tema ,Long> {
+	
+	public List<Tema> findAllByAssuntoContainingIgnoreCase(String assunto);
+
+}
