88063c2cbb5dd4aad9a1bb3cb991becdb2038ae0
Matheus <matheus.mfa10@gmail.com>
2022-02-01 15:25:35 -0300

    @Matheus-Anjos [Task : UsuarioRepository e Service]

---
+++ src/main/java/org/generation/blog/service/UsuarioService.java
@@ -0,0 +1,50 @@
+package org.generation.blog.service;
+
+import java.nio.charset.Charset;
+import java.util.Optional;
+
+import org.generation.blog.models.UserLogin;
+import org.generation.blog.models.Usuario;
+import org.generation.blog.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.apache.commons.codec.binary.Base64;
+
+@Service
+public class UsuarioService {
+	
+	@Autowired
+	private UsuarioRepository repository;
+	
+	public Usuario CadastrarUsuario( Usuario usuario) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		
+		String senhaEncoder = encoder.encode(usuario.getSenha());
+		usuario.setSenha(senhaEncoder);
+		
+		return repository.save(usuario);
+		
+	}
+	
+	public Optional<UserLogin> Logar(Optional<UserLogin> user){
+	BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+	Optional<Usuario> usuario = repository.findByUsuario(user.get().getUsuario());
+		
+		if(usuario.isPresent()) {
+			if(encoder.matches(user.get().getSenha(), usuario.get().getSenha())) { 
+				String auth =  user.get().getUsuario() + ":" + usuario.get().getSenha();
+				byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
+				String authHeader = "Basic" + new String(encodedAuth);
+				user.get().setToken(authHeader);
+				user.get().setNome(usuario.get().getNome());
+				return user;
+		}
+		
+	}
+		
+		return null;
+	}
+	
+
+}
